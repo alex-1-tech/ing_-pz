a9ffcac95ca8bdedbd231ccca0183fbccb476805
Alex <chistovreal@gmail.com>
2024-08-17 16:36:04 +0300

    change Cmake for Qt

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include "first_task.h"
 #include "second_task.h"
+#include "third_task.h"
 #include <iostream>
 
-int main() {
+int main(int argc, char *argv[]) {
     std::cout << "Enter the issue number: ";
     int number;
     std::cin >> number;
     if (number == 1) first_task::solution();
     else if (number == 2) second_task::solution();
+    else if (number == 3) third_task::solution(argc, argv);
+    return 0;
 }
+
